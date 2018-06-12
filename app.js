b9f33d4d41475b8fd0a4ceebb06a4a7d1fd915c3
UrbanRude <urban.rude@gmail.com>
2018-06-11 21:37:46 -0500

    Utilizando promesas

--- app.js
+++ app.js
@@ -24,36 +24,34 @@ let salarios = [
     }
 ];
 
-let getEmpleado = ( id, callback ) => {
-    let empleadoDB = empleados.find( empleado => empleado.id === id );
-    if ( !empleadoDB ){
-        callback(`No existe un empleado con el ID ${id}`);
-    }else{
-        callback( null, empleadoDB);
-    }
-};
-
-let getSalario = ( empleado, callback ) => {
-    let salarioDB = salarios.find( salario => salario.id === empleado.id);
-    if( !salarioDB ){
-        callback(`No se encontro un salario para el usuario ${empleado.nombre}`);
-    }else{
-        callback( null, {
-            nombre: empleado.nombre,
-            salario: salarioDB.salario,
-            id: empleado.id
-        } );
-    }
-};
-
-getEmpleado( 3, ( err,empleado ) => {
-    if( err ){
-        return console.log(err);
-    }
-    getSalario( empleado, (err,respuesta) => {
-        if( err ){
-            return console.log(err);
+let getEmpleado = ( id ) => {
+    return new Promise( (resolve,reject) => {
+        let empleadoDB = empleados.find( empleado => empleado.id === id );
+        if( !empleadoDB ){
+            reject(`No existe un empleado con el ID ${id}`);
+        }else{
+            resolve(empleadoDB);
         }
-        console.log(`El salario de ${respuesta.nombre} es de ${respuesta.salario}`);
-    } );
-});
+    }); 
+};
+
+let getSalario = ( empleado ) => {
+    return new Promise( (resolve,reject) => {
+        let salarioDB = salarios.find( salario => salario.id === empleado.id );
+        if( !salarioDB ){
+            reject(`No se encuntra el salario para ${empleado.nombre}`);
+        }else{
+            resolve( {
+                nombre: empleado.nombre,
+                salario: salarioDB.salario,
+                id: empleado.id             
+            } );
+        }
+    }); 
+}
+
+getEmpleado(3).then( empleado => {
+    getSalario(empleado).then( resp => {
+        console.log(`El salario de ${resp.nombre} es de ${resp.salario}`);
+    }, err => console.log(err));
+}, ( err ) => console.log(err));
