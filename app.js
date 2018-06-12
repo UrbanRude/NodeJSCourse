ba232b2c7f0e205e41bd543608b71b5be4e93d3f
UrbanRude <urban.rude@gmail.com>
2018-06-11 21:47:53 -0500

    Encadenando Promesas

--- app.js
+++ app.js
@@ -50,8 +50,10 @@ let getSalario = ( empleado ) => {
     }); 
 }
 
-getEmpleado(3).then( empleado => {
-    getSalario(empleado).then( resp => {
+getEmpleado(10).then( empleado => {
+        return getSalario( empleado );
+    }).then( resp => {
         console.log(`El salario de ${resp.nombre} es de ${resp.salario}`);
-    }, err => console.log(err));
-}, ( err ) => console.log(err));
+    }).catch( err => {
+        console.log( err );
+    });
