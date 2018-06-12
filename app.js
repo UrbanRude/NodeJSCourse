0093ac5c882779d3f71d859222a769c51c5a5fc1
UrbanRude <urban.rude@gmail.com>
2018-06-11 23:00:16 -0500

    Async await avanzados

--- app.js
+++ app.js
@@ -24,34 +24,33 @@ let salarios = [
     }
 ];
 
-let getEmpleado = ( id ) => {
-    return new Promise( (resolve,reject) => {
-        let empleadoDB = empleados.find( empleado => empleado.id === id );
-        if( !empleadoDB ){
-            reject(`No existe un empleado con el ID ${id}`);
-        }else{
-            resolve(empleadoDB);
-        }
-    }); 
+let getEmpleado = async( id ) => {
+    let empleadoDB = empleados.find( empleado => empleado.id === id );
+    if( !empleadoDB ){
+        throw new Error(`No existe un empleado con el ID ${id}`);
+    }else{
+        return empleadoDB;
+    }
 };
 
-let getSalario = ( empleado ) => {
-    return new Promise( (resolve,reject) => {
-        let salarioDB = salarios.find( salario => salario.id === empleado.id );
-        if( !salarioDB ){
-            reject(`No se encuntra el salario para ${empleado.nombre}`);
-        }else{
-            resolve( {
-                nombre: empleado.nombre,
-                salario: salarioDB.salario,
-                id: empleado.id             
-            } );
-        }
-    }); 
+let getSalario = async( empleado ) => {
+    let salarioDB = salarios.find( salario => salario.id === empleado.id );
+    if( !salarioDB ){
+        throw new Error(`No se encuntra el salario para ${empleado.nombre}`);
+    }else{
+        return {
+            nombre: empleado.nombre,
+            salario: salarioDB.salario,
+            id: empleado.id             
+        } 
+    }
 }
 
-getEmpleado(3).then( empleado => {
-    getSalario(empleado).then( resp => {
-        console.log(`El salario de ${resp.nombre} es de ${resp.salario}`);
-    }, err => console.log(err));
-}, ( err ) => console.log(err));
+let getInformacion = async(id) => {
+    let empleado = await getEmpleado(id);
+    let resp = await getSalario(empleado);    
+    return `${resp.nombre} tiene un salario de $${resp.salario}`
+}
+
+getInformacion(3).then( mensaje => console.log(mensaje) )
+                 .catch( err => console.log(err));
