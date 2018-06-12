1b22cb67bd52ef8c5a350a54ccdf138a16f6dc8f
UrbanRude <urban.rude@gmail.com>
2018-06-11 21:06:09 -0500

    Calback avanzados

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
-
-// Callback sencillo
-// setTimeout( () => {
-//     console.log('Hola mundo!');
-// }, 3000 );
-
-let getUserById = ( id, callback ) => {
-    let usuario = {
-        nombre : 'Urbano',
-        id
+let empleados = [
+    {
+        id:1,
+        nombre:'Urbano'
+    },
+    {
+        id:2,
+        nombre:'Martha'
+    },
+    {
+        id:3,
+        nombre:'Jose'
     }
-    if( id === 10 ){
-        callback(`El usuario con id ${ id }, no existe en la BD`);
+];
+
+let salarios = [
+    {
+        id:1,
+        salario:100
+    },
+    {
+        id:2,
+        salario:200
+    }
+];
+
+let getEmpleado = ( id, callback ) => {
+    let empleadoDB = empleados.find( empleado => empleado.id === id );
+    if ( !empleadoDB ){
+        callback(`No existe un empleado con el ID ${id}`);
     }else{
-        callback( null,usuario );
+        callback( null, empleadoDB);
     }
-}
+};
 
-getUserById( 1 , ( err,usuario ) => {
+let getSalario = ( empleado, callback ) => {
+    let salarioDB = salarios.find( salario => salario.id === empleado.id);
+    if( !salarioDB ){
+        callback(`No se encontro un salario para el usuario ${empleado.nombre}`);
+    }else{
+        callback( null, {
+            nombre: empleado.nombre,
+            salario: salarioDB.salario,
+            id: empleado.id
+        } );
+    }
+};
+
+getEmpleado( 3, ( err,empleado ) => {
     if( err ){
         return console.log(err);
     }
-        console.log('Usuario de BD',usuario);
-})
+    getSalario( empleado, (err,respuesta) => {
+        if( err ){
+            return console.log(err);
+        }
+        console.log(`El salario de ${respuesta.nombre} es de ${respuesta.salario}`);
+    } );
+});
