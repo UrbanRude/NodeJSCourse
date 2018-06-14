3ef50b3422a5e5ceb69222940cce8c15cb14daa9
UrbanRude <urban.rude@gmail.com>
2018-06-14 00:17:00 -0500

    Utilizando yargs para el uso de comandos

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 
 
+const argv = require('yargs')
+                .command('listar','Imprime en consola la tabla de multiplicar', {
+                    base: {
+                        demand: true,
+                        alias: 'b'
+                    },
+                    limite:{
+                        alias: 'l',
+                        default: 10
+                    }
+                })
+                .help()
+                .argv; 
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
+console.log(argv.base);
+console.log(argv.limite);
 
-crearArchivo( base )
-     .then( archivo => console.log(`El archivo tabla-${ base }.txt ha sido creado`) )
-     .catch( err => console.log( err ));
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+
+// crearArchivo( base )
+//      .then( archivo => console.log(`El archivo tabla-${ base }.txt ha sido creado`) )
+//      .catch( err => console.log( err ));
